b4661dbeedf03fd377811e53793632ce6a59799a
Yesid Rivera <ripyesid33@gmail.com>
2024-12-13 17:08:56 -0500

    create a model User on home app

--- my_personal_page/home/models.py
+++ my_personal_page/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    userName = models.CharField(max_length = 255)
+    firstName = models.CharField(max_length = 255)
+    lastName = models.CharField(max_length = 255)
+    password = models.CharField(max_length = 100)
